a70a707424d7df3ea375a12c47a1aaa00ca57029
Victor Barros Melo <33198775+vbmelo@users.noreply.github.com>
2022-02-01 18:11:44 +0000

    erasing evicene

--- semaphore_airLift/src/probConst.h
+++ semaphore_airLift/src/probConst.h
@@ -11,59 +11,55 @@
 #ifndef PROBCONST_H_
 #define PROBCONST_H_
 
+// FICHEIRO: probConst.h
+
 /* Generic parameters */
 
-/** \brief number of passengers */
-#define  N        21 
+#define N 18                        /** \brief number of passengers */
 
-/** \brief min flight capacity */
-#define  MINFC     5 
+#define MINFC 5                     /** \brief min flight capacity */
 
-/** \brief max flight capacity */
-#define  MAXFC    10
+#define MAXFC 10                    /** \brief max flight capacity */
 
-/** \brief max flight capacity */
-#define  MAXNF    10
+#define MAXNF 20                    /** \brief max flight capacity */
 
-/** \brief max flight capacity */
-#define  MAXTRAVEL   30000.0 
+#define MAXTRAVEL 45000.0           /** \brief max flight capacity */
 
-/** \brief max flight capacity */
-#define  MAXFLIGHT   2000.0 
+#define MAXFLIGHT 12983.0            /** \brief max flight capacity */
 
 /* Pilot state constants */
 
 /** \brief pilot flying to starting airport */
-#define  FLYING_BACK                  0
+#define FLYING_BACK 0
 /** \brief pilot signals ready for boarding */
-#define  READY_FOR_BOARDING           1
+#define READY_FOR_BOARDING 1
 /** \brief pilot wait for boarding to complete */
-#define  WAITING_FOR_BOARDING         2
+#define WAITING_FOR_BOARDING 2
 /** \brief pilot takes passengers to destination */
-#define  FLYING                       3
+#define FLYING 3
 /** \brief pilot drops passengers at destination */
-#define  DROPING_PASSENGERS           4
+#define DROPING_PASSENGERS 4
 
 /* Hostess state constants */
 
 /** \brief hostess waits for plane to be ready for boarding */
-#define  WAIT_FOR_FLIGHT              0
+#define WAIT_FOR_FLIGHT 0
 /** \brief hostess waits for passenger to arrive */
-#define  WAIT_FOR_PASSENGER           1
+#define WAIT_FOR_PASSENGER 1
 /** \brief hostess checks passenger passport */
-#define  CHECK_PASSPORT               2
+#define CHECK_PASSPORT 2
 /** \brief hostess signals boarding is complete */
-#define  READY_TO_FLIGHT              3
+#define READY_TO_FLIGHT 3
 
 /* Passenger state constants */
 
 /** \brief passenger is going to the airport */
-#define  GOING_TO_AIRPORT             0
+#define GOING_TO_AIRPORT 0
 /** \brief passenger is waiting in queue */
-#define  IN_QUEUE                     1
+#define IN_QUEUE 1
 /** \brief passenger is flying */
-#define  IN_FLIGHT                    2
+#define IN_FLIGHT 2
 /** \brief passenger arrives at destination */
-#define  AT_DESTINATION               3
+#define AT_DESTINATION 3
 
 #endif /* PROBCONST_H_ */
